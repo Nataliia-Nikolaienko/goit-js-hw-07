8be135015016cfe2c5648e43bb8e931449732503
Nataliia-Nikolaienko <natalina_pretty@ukr.net>
2022-11-13 20:34:47 +0200

    total changes

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,4 @@
-// galleryItems масив обєктів
+
 
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
@@ -10,7 +10,7 @@ const imagesMarkup = createGalleryMarkup(galleryItems);
 
 galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+galleryContainer.addEventListener('click', onClickModalWindow);
 
 function createGalleryMarkup(galleryItems) {
     return galleryItems
@@ -33,55 +33,33 @@ function createGalleryMarkup(galleryItems) {
 };
 
 
-
-function onGalleryContainerClick(evt) {
-    evt.preventDefault();
-
-    if (!evt.target.classList.contains('gallery__image')) {
-        return;
-    }
-    
-    console.log(evt.target.dataset.source);
-
-  const instance = basicLightbox.create(
+function createModalWindow(imageAdress) {
+  
+    window.instance = basicLightbox.create(
     `
-    <img src="${evt.target.dataset.source}"/>
-    `)
-  instance.show();
+    <img src="${imageAdress}"/>
+    `,
+    {
+      onShow: () => 
+        window.addEventListener('keydown', onCloseModalWindowByEsc),
+      onClose: () => 
+        window.removeEventListener('keydown', onCloseModalWindowByEsc),
+    }
+  );
+  return instance;
 }
 
+function onClickModalWindow(event) {
+event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  const origImages = event.target.dataset.source;
+  createModalWindow(origImages).show();
+};
 
-
-
-// Пробувала ще так але десь роблю помилку і модалка не відкривається і не закривається
-
-
-// function onGalleryContainerClick(evt) {
-//     evt.preventDefault();
-
-//     if (!evt.target.classList.contains('gallery__image')) {
-//         return;
-//     }
-    
-//     console.log(evt.target.dataset.source);
-
-//   const instance = basicLightbox.create(
-//     `
-//     <img src="${evt.target.dataset.source}"/>
-//     `,
-//     {
-//       onShow: () => 
-//         window.addEventListener('keydown', onCloseModalWindowByEsc),
-//       onClose: () => 
-//         window.removeEventListener('keydown', onCloseModalWindowByEsc),
-//     }
-//   );
-//   return instance;
-// }
-
-
-// const onCloseModalWindowByEsc = (event) => {
-//   if (event.code === 'Escape') {
-//     window.removeEventListener('keydown', onGalleryContainerClick)
-//   }
-// }
+function onCloseModalWindowByEsc(event) {
+  if (event.code === 'Escape') {
+    instance.close();
+  }
+};
